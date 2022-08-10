bd3abd6b2a7bb626abde60fd78c8c8381d350e3b
João Victor Pires Almeida Campos <joao.victor_55@hotmail.com>
2022-08-10 14:36:33 -0300

    feat(todo): add drag and drop

--- src/app/pages/todo/todo.component.ts
+++ src/app/pages/todo/todo.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Todo } from 'src/app/shared/interfaces/todo';
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
+  todo: Todo[] = [
+    { title: 'Get to work', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+    { title: 'Pick up groceries', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+    { title: 'Go home', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+    { title: 'Fall asleep', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }
+  ];
+
+  done = [
+    { title: 'Get up', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+    { title: 'Brush teeth', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+    { title: 'Take a shower', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' },
+    { title: 'Check e-mail', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.' }
+  ];
+;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  drop(event: any) {
+    console.log(event)
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 
 }
