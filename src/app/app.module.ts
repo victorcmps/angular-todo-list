274fb1a9734b52d8fc7405a539ab4b45236cacc2
João Victor Pires Almeida Campos <joao.victor_55@hotmail.com>
2022-08-09 21:48:45 -0300

    feat(todo): add side menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { TodoComponent } from './components/todo/todo.component';
+import { SidebarComponent } from './core/components/sidebar/sidebar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SidebarComponent
+  ],
   imports: [
     CommonModule
   ]
